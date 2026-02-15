922440e11569a8c66f3992628146ee6e2f5f2d17
viveroa2291 <viveroa2291@uwec.edu>
2026-02-14 21:07:47 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import me from './images/me.jpeg';
+import us from './images/us.JPG';
 function App() {
   const [envelopeState, setEnvelopeState] = useState({
     isInnerOpen: false,
@@ -33,24 +34,46 @@ function App() {
       isCloseBtnVisible: false,
       isMessage1Hidden: false,
       isMessage2Visible: false,
-      isImageVisible: false
+      isImageVisible: false,
+      isSecondPageVisible: false,
   }));
 }
+  const nextPage = () => {
+    setEnvelopeState((prevState) => ({
+      ...prevState,
+      isMessage2Visible: false,
+      isSecondPageVisible: true,
+      selectedUs: us,
+    }));
+  }
+  const backPage = () => {
+    setEnvelopeState((prevState) => ({
+      ...prevState,
+      isMessage2Visible: true,
+      isSecondPageVisible: false,
+      selectedUs: us
+    }));
+  };
   return (
     <div className="App">
       <article>
         <div className="envelope-container">
           <p className={`message message-1 ${envelopeState.isMessage1Hidden ? 'hide' : ''}`}>
-            <strong>Happy Valentine's Day!</strong>
-            <br/><br/>Click on the heart seal to open the envelope.
+            <br/>Click on the heart seal to open the envelope.
           </p>
           <span className={`note ${envelopeState.isMessage2Visible ? 'show' : 'hide'} `}>
             <span className='picture'>
               <img className={`imageChosen ${envelopeState.isImageVisible ? 'show' : ''}`} src={envelopeState.selectedImage} alt=''/>
             </span>
-            <p className={`message message-2 ${envelopeState.isMessage2Visible ? 'show' : ''}`}>Hello Mikayla,</p>
-            <p className={`valentines-message message-2 ${envelopeState.isMessage2Visible ? 'show' : ''}`}>Cheers to all the crazy fun we've had. Your friendship means the world to me, just like a flawless code in a sea of bugs. Looking forward to more! Happy Valentine's Day! 💌</p>
-            <p className={`sincerely message-2 ${envelopeState.isMessage2Visible ? 'show' : ''}`}>Love, Adan</p>
+            <p className={`message message-2 ${envelopeState.isMessage2Visible ? 'show' : ''}`}>Hi Khiley,</p>
+            <p className={`valentines-message message-2 ${envelopeState.isMessage2Visible ? 'show' : ''}`}>I'm very glad I got a chance to meet you. I really enjoy my time that I spend with you and would like to get to know you more this year :). You're quite an exceptional woman that I've ever met and I truly enjoy our conversations that we have and find you to be quite remarkable and very smart. I hope you enjoy your time in Chicago this weekend and look forward to seeing you!</p>
+            <button className={`next-button`} onClick={nextPage}>Next</button>
+          </span>
+          <span className={`note ${envelopeState.isSecondPageVisible ? 'show' : 'hide'} `}>
+            <img className={'us-picture'} src={envelopeState.selectedUs} alt='us'/>
+            <p className={'Happy-valentines'}>Happy Valentine's Day! 💌</p>
+            <p className={`sincerely`}>Love, Adan :)</p>
+            <button className={`back-button ${envelopeState.isHeartBtnHidden ? 'hide' : 'show'}`} onClick={backPage}>Back</button>
           </span>
           <button className={`heart-btn otherButton ${envelopeState.isHeartBtnHidden ? 'hide' : 'show'}`} onClick={openEnvelope}>&#10084;</button>
           <button className={`close-btn otherButton ${envelopeState.isCloseBtnVisible ? 'show' : ''}`} onClick={closeEnvelope}>close envelope</button>
